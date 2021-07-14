92674faa8c755c3111398beea667c30145a666cb
Ian Shields <ian.p.shields@gmail.com>
2021-07-14 09:21:07 -0400

    Fix bug in which titlify replaced the original string

--- lib/titlify/string_extensions.rb
+++ lib/titlify/string_extensions.rb
@@ -5,7 +5,7 @@ module Titlify
   module StringExtensions
     def self.included(base)
       Titlify.methods(false).each do |method|
-        eval("def #{method}(*args, &block); Titlify.#{method}(self, *args, &block); end")
+        eval("def #{method}(*args, &block); Titlify.#{method}(self.clone, *args, &block); end")
         eval("def #{method}!(*args, &block); replace Titlify.#{method}(self, *args, &block); end")
       end
     end
